5416011a49c9bb16d146535ff3e2ee333940a18d
Amil Masic <amil.masic@gmail.com>
2019-06-25 08:53:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,9 @@ class App < Sinatra::Base
   end
 
   get '/multiply/:num1/:num2' do
-    product = num1*num2
-    # product
+    num1 = params[:num1]
+    num2 = params[:num2]
+    product = num1 * num2
+
   end
 end
